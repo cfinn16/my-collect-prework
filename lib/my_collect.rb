79c9e8cbde055b5ddf0b4756cb71190d555e43f6
Connor Finnegan <connorfinnegan16@gmail.com>
2018-10-16 16:03:43 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,10 @@
 def my_collect(collection)
-  i = 0 
-  while i < collection.length 
-    yield(collection[i])
-    i += 1 
-  end
-  collection
+  if block_given?
+    i = 0 
+    while i < collection.length 
+      yield(collection[i])
+      i += 1 
+    end
+    collection
 end
 
